929ed1032bfcb211b6a1fcf25329eba06d0df4e3
Eduardo M. Martins <105308925+Eduardo-M-Martins@users.noreply.github.com>
2023-09-29 21:43:00 +0000

    export

--- custom-scripts/pre-build.sh
+++ custom-scripts/pre-build.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# export LINUX_OVERRIDE_SRCDIR=/workspaces/linux-4.13.9
+
 # Configuração de rede
 cp $BASE_DIR/../custom-scripts/S41network-config $BASE_DIR/target/etc/init.d
 chmod +x $BASE_DIR/target/etc/init.d/S41network-config
